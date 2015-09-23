916fe65f464aa7729dd4256404849f5334dc8acd
Caio SBA <caiosba@gmail.com>
2015-09-23 20:55:54 -0300

    Ticket #4229: Fixing bug and fixing tests

--- lib/watch_job.rb
+++ lib/watch_job.rb
@@ -1,3 +1,5 @@
+require 'watchbot_memory'
+
 class WatchJob < Struct.new(:link)
   def perform
     link.check unless link.nil?
@@ -12,11 +14,11 @@ class WatchJob < Struct.new(:link)
   end
 
   def after
-    cmd = 'cd ' + Rails.root + ' && RAILS_ENV=production bin/delayed_job -n5 stop && RAILS_ENV=production bin/delayed_job -n5 start'
+    cmd = 'cd ' + Rails.root.to_s + ' && RAILS_ENV=production bin/delayed_job -n5 stop && RAILS_ENV=production bin/delayed_job -n5 start'
     memory = Watchbot::Memory.value
     limit = ENV['WATCHBOT_DELAYED_JOB_MEMORY_LIMIT'] || 1000000000
-    if Rails.env.production? && memory >= limit 
-      puts "Reached #{memory} bytes of memory, restarting..."
+    if memory >= limit.to_i
+      Delayed::Worker.logger.debug("Reached #{memory} bytes of memory, restarting...")
       system(cmd)
     end
   end
