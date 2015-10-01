0f9c5ba3607f79f620a0651d9a932829296b7b70
Caio SBA <caiosba@gmail.com>
2015-10-01 03:16:26 -0300

    Ticket #4226: Should configure both Redis client and server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,7 @@
 sidekiq = YAML.load_file(File.join(Rails.root, 'config', 'sidekiq.yml'))
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://#{sidekiq[:redis_host]}:#{sidekiq[:redis_port]}/#{sidekiq[:redis_database]}", namespace: "sidekiq_watchbot_#{Rails.env}" }
+end
 Sidekiq.configure_client do |config|
   config.redis = { url: "redis://#{sidekiq[:redis_host]}:#{sidekiq[:redis_port]}/#{sidekiq[:redis_database]}", namespace: "sidekiq_watchbot_#{Rails.env}" }
 end
