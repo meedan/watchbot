7e2284d6151b881b24de87aae39bf60566c450d1
Caio SBA <caiosba@gmail.com>
2015-10-02 02:49:01 -0300

    Ticket #4237: Adding script to calculate number of jobs in each priority queue

--- scripts/count.rb
+++ scripts/count.rb
@@ -1,5 +1,7 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__) + '/../config/environment'
 q = { 'lowest' => 0, 'low' => 0, 'average' => 0, 'high' => 0, 'default' => 0 }
 Sidekiq::Cron::Job.all.collect do |j|
-  q[j.instance_variable_get(:@queue)]
+  q[j.instance_variable_get(:@queue)] += 1
 end
 puts q.inspect
