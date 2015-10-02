f7d32f2831d8f94f1d217b047b77bfa96feb7fa5
Caio SBA <caiosba@gmail.com>
2015-10-02 16:22:41 -0300

    Ticket #4237: The script that counts the number of jobs in each queue also counts the number of unqueued jobs

--- scripts/count.rb
+++ scripts/count.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require File.dirname(__FILE__) + '/../config/environment'
-q = { 'lowest' => 0, 'low' => 0, 'average' => 0, 'high' => 0, 'default' => 0 }
+q = { 'lowest' => 0, 'low' => 0, 'average' => 0, 'high' => 0, 'default' => 0, 'not-started' => 0 }
 Sidekiq::Cron::Job.all.collect do |j|
-  q[j.instance_variable_get(:@queue)] += 1
+  j.last_enqueue_time.nil? ? q['not-started'] += 1 : q[j.instance_variable_get(:@queue)] += 1
 end
 puts q.inspect
